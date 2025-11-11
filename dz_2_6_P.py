4b4eef9e530e0283a9481fcf6a1a67b869a24bab
Elenie <dr.elena.shved@gmail.com>
2025-11-11 09:37:21 +0200

    Fix spelling errors and correct perimeter calculation

--- dz_2_6_P.py
+++ dz_2_6_P.py
@@ -1,6 +1,6 @@
 lenght=float(input("Enter the length of rectangle: "))
-wight=float(input("Enter the width of rectangle: "))
-perimetr= 2*lenght+wight
+widht=float(input("Enter the width of rectangle: "))
+perimetr= 2*(lenght+widht)
 print(perimetr)
 
 
