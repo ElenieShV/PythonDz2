97271cb5cc311c69d7f1f7aa14c8cf2edf7c33fa
Elenie <dr.elena.shved@gmail.com>
2025-11-11 09:35:41 +0200

    Fix time conversion to include minutes

--- dz_2_3_hours.py
+++ dz_2_3_hours.py
@@ -1,3 +1,4 @@
 total_minutes=int(input("Enter number of minutes:"))
-hours = total_minutes / 60
-print("Hours", hours)
+hours = total_minutes // 60
+minutes = total_minutes % 60
+print(hours, "Hours", minutes, "Minutes")
